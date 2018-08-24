b519b49271470e930c0be7b1a0083f8b96e36f03
maximecastelli <maximecastelli@yahoo.fr>
2018-08-24 10:37:47 +0200

    update to electron 2.0.8

--- src/script.js
+++ src/script.js
@@ -1,5 +1,28 @@
+var app = require('electron').remote;
+var dialog = app.dialog;
+var fs = require('fs');
+
 console.log("LOADED");
 
 const fontManager = require('font-manager');
 var fonts = fontManager.getAvailableFontsSync();
-console.log(fonts);
+console.log('system fonts loaded…');
+
+///
+//Export file
+
+document.getElementById('export').onclick = () =>{
+  dialog.showSaveDialog((fileName) =>{
+    if(fileName === undefined){
+      alert("Nom de fichier non-défini");
+      return;
+    }
+
+    var content = "Hello";
+
+    fs.writeFile(fileName, content, (err) => {
+      if (err) console.log(err);
+      alert("votre fichier à été correctement enregistré");
+    } )
+  });
+};
